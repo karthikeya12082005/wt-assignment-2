5efb4172c777d02b1a518f0904cf1e0bd2277bae
karthikeya12082005 <karthikeya.kasam@gmail.com>
2025-04-20 18:58:43 +0530

    Update studentRoutes.js

--- student-management-backend/routes/studentRoutes.js
+++ student-management-backend/routes/studentRoutes.js
@@ -8,29 +8,19 @@ const {
   deleteStudent
 } = require('../controllers/studentController');
 
-// Add route logging for debugging
-router.use((req, res, next) => {
-  console.log(`Incoming ${req.method} request to ${req.path}`);
-  next();
-});
-
 // GET all students
-router.route('/')
-  .get(getStudents)
-  .post(createStudent);
+router.get('/', getStudents);
 
-// Student ID parameter validation middleware
-router.param('id', (req, res, next, id) => {
-  if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-    return res.status(400).json({ error: 'Invalid ID format' });
-  }
-  next();
-});
+// GET single student
+router.get('/:id', getStudentById);
 
-// Single student operations
-router.route('/:id')
-  .get(getStudentById)
-  .put(updateStudent)
-  .delete(deleteStudent);
+// POST create student
+router.post('/', createStudent);
+
+// PUT update student
+router.put('/:id', updateStudent);
+
+// DELETE student
+router.delete('/:id', deleteStudent);
 
 module.exports = router;
