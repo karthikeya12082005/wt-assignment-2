024336ac0af6919c80810cf18915079cd7288793
karthikeya12082005 <karthikeya.kasam@gmail.com>
2025-04-20 16:45:54 +0530

    Update db.js

--- student-management-backend/config/db.js
+++ student-management-backend/config/db.js
@@ -1,13 +1,8 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
